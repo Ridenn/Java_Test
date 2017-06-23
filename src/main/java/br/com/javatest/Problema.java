ef8f9ab7bb18d06aae9654f339eef015151a15c0
Lucas <lucaspedrosa11@hotmail.com>
2017-06-23 19:45:13 -0300

    Minor changes
    - Method executaAcao() changed name to chamaAcao() to fit better in the
    situation

--- src/main/java/br/com/javatest/Problema.java
+++ src/main/java/br/com/javatest/Problema.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class Problema {
 
-	public static String executaAcao(String comando){
+	public static String chamaAcao(String comando){
 		String comandoFinal = "";
 		String oldCommand = comando;
 		
+		while(oldCommand.length() != 0){
+			comando = oldCommand.substring(0, 1);
+			oldCommand = oldCommand.substring(1, oldCommand.length());
+			
+			executaAcao(comando);
+		}
 		
 		return comandoFinal;
 	}
@@ -19,7 +25,7 @@ public class Problema {
 		String commands = input.next();
 		input.close();
 		
-		executaAcao(commands);
+		chamaAcao(commands);
 	}
 
 }
