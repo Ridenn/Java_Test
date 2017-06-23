14a6e1daa551f56f287972a2769fefa6231fd7c4
Lucas <lucaspedrosa11@hotmail.com>
2017-06-23 20:33:01 -0300

    Commands created
    - Created switch to control the commands.

--- src/main/java/br/com/javatest/Problema.java
+++ src/main/java/br/com/javatest/Problema.java
@@ -1,5 +1,7 @@
 package br.com.javatest;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Problema {
@@ -7,10 +9,32 @@ public class Problema {
 	public static String chamaAcao(String comando){
 		String comandoFinal = "";
 		String oldCommand = comando;
+		int x, y, z = 0;
+		int aux = 0;
 		
 		while(oldCommand.length() != 0){
-			comando = oldCommand.substring(0, 1);
+			comando = oldCommand.substring(0, 1).toUpperCase();
 			oldCommand = oldCommand.substring(1, oldCommand.length());
+			
+			switch (comando) {
+			case "L":
+				
+				break;
+			case "R":
+				
+				break;
+			case "M":
+				
+				break;
+			case "U":
+				
+				break;
+			case "D":
+				
+				break;
+			default:
+				break;
+			}
 			
 			executaAcao(comando);
 		}
@@ -19,6 +43,11 @@ public class Problema {
 	}
 	
 	
+	private static void executaAcao(String comando) {
+		
+	}
+
+
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
