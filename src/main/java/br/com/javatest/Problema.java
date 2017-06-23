31adf21d0f007388332bcd1a9bc0114fe11a5ca2
Lucas <lucaspedrosa11@hotmail.com>
2017-06-23 19:37:46 -0300

    Method creation
    - Created method executaAcao to execute the action asked by the user.

--- src/main/java/br/com/javatest/Problema.java
+++ src/main/java/br/com/javatest/Problema.java
@@ -1,10 +1,25 @@
 package br.com.javatest;
 
+import java.util.Scanner;
+
 public class Problema {
 
+	public static String executaAcao(String comando){
+		String comandoFinal = "";
+		String oldCommand = comando;
+		
+		
+		return comandoFinal;
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		String commands = input.next();
+		input.close();
+		
+		executaAcao(commands);
 	}
 
 }
