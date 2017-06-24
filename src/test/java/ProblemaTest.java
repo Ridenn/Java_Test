1ca39a3886593bceecb12c5e1e397b75dccd214d
Lucas <lucaspedrosa11@hotmail.com>
2017-06-23 22:03:06 -0300

    Added document with explanations and changed test class
    - Added document with some explanations to the root path;
    - Added all test cases to the test class.

--- src/test/java/ProblemaTest.java
+++ src/test/java/ProblemaTest.java
@@ -9,8 +9,8 @@ public class ProblemaTest extends TestCase{
  
 	@Test
 	public void testchamaAcao(){
-		String comando = "LMRDDMMUU";
-		String retornoEsperado = "-1 2 0 NORTE";
+		String comando = "LMMMRMRRMMDDDU";//teste1: LMRDDMMUU / teste2: RMMLMMMDDLL / teste3: LMMMRMRRMMDDDU
+		String retornoEsperado = "-3 -1 -2 SUL";//retorno1: -1 2 0 NORTE / retorno2: 2 3 -2 SUL / retorno3: -3 -1 -2 SUL
 		
 		String retornoFeito = Problema.chamaAcao(comando);
 		
